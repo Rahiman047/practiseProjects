27b97dff1a0452a331aaa9758b107a733505cbe7
Rahiman <abdurrahiman100@gmail.com>
2023-09-20 01:08:32 +0530

    axios get

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+const url = "https://course-api.com/react-store-products";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userData, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const responseData = await axios.get(url);
+      const data = responseData.data;
+      setData(data);
+      // console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
 
   useEffect(() => {
-    console.log("Hello");
-  }, [count]);
+    fetchData();
+  }, []);
+
+  const showJsx = (eachData) => {
+    console.log(eachData);
+    const { id, company, description, name } = eachData;
+    return (
+      <>
+        <h1>{company}</h1>
+        <p>{company}</p>
+        <p>{description}</p>
+        <p>{name}</p>
+      </>
+    );
+  };
 
   return (
     <>
-      <div>
-        <h1>{`count: ${count}`}</h1>
-        <div>
-          <button type="button" onClick={() => setCount(count + 1)}>
-            Click ME
-          </button>
-        </div>
-      </div>
+      {/* <h1>Hello</h1> */}
+      {userData.map((eachData) => {
+        return showJsx(eachData);
+      })}
     </>
   );
 }

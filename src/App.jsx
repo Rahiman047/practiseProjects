9b1d7f1ee22e3dd64b37b3bbf57a7e2d7b17d7c5
Rahiman <abdurrahiman100@gmail.com>
2023-09-22 00:11:33 +0530

    multiple return

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,18 @@
 import { useState, useEffect } from "react";
-import axios from "./Axios.jsx";
+
 import "./App.css";
-import ShowJsx from "./assets/Show";
-import authFetch from "./AuthFetch.jsx";
-const url = "https://course-api.com/react-store-products";
 
 function App() {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    try {
-      const getRes = await authFetch.get("react-store-products");
-      const resData = await getRes.data;
-      setData(resData);
-    } catch (error) {
-      console.log(error.response);
-    }
-  };
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getData();
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
   }, []);
 
   return (
-    <>
-      <h1>Axios</h1>
-      <div>
-        {data.map((eachData) => {
-          return (
-            <div key={eachData.id}>
-              <h1>{eachData.company}</h1>
-              <p>{eachData.description}</p>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div>{(isLoading && <h1>Loading...</h1>) || <h1>Loading Done</h1>}</div>
   );
 }
 

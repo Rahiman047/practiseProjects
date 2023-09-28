81ea3add5d767d67932cfacc2053157b4c678203
Rahiman <abdurrahiman100@gmail.com>
2023-09-28 17:47:38 +0530

    controlled

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,43 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const submitClicked = (e) => {
+    e.preventDefault();
+
+    setName("");
+    setEmail("");
+  };
+
   return (
     <div>
-      <form className="form-cont">
+      <form className="form-cont" onSubmit={submitClicked}>
         <h1 className="form-heading">Controlled Inputs</h1>
         <div className="form-input-comp">
           <label className="form-label" htmlFor="name">
             Name:-
           </label>
-          <input type="text" id="name" className="form-input" />
+          <input
+            type="text"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="form-input-comp">
           <label className="form-label" htmlFor="email">
             Email:-
           </label>
-          <input type="text" id="email" className="form-input" />
+          <input
+            type="text"
+            id="email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="submit-btn-cont">
           <button type="submit" className="submit-btn">
